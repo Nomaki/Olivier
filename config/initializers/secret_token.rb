0feabad20c8bdbe567022521c084e00ffe206be6
Nomaki <68727@supinfo.com>
2011-08-07 17:29:23 +0200

    start with olivier website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Olivier::Application.config.secret_token = '089d159dd9e5be25ae5e8a3641c145dcdba58d5debb6894b76c024027c6fc22e9efc95ba1d19dd22c8f451ad6273b8f12a60bbe2a184f58814de108536a230d5'
